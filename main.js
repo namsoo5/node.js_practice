88fe7db73ae1bedbbafa48b22ae58a3e093f481b
namsoo5 <35462671+namsoo5@users.noreply.github.com>
2018-08-13 18:10:59 +0900

    connect is localhost:3000

---
+++ main.js
@@ -0,0 +1,137 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var template = require('./lib/template.js')
+var path = require('path');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){  //초기화면
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'welcome!';
+          var description = 'hello, node.js';
+          var list = template.list(filelist);
+          var html = template.html(title, list, `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>`
+          );
+        response.writeHead(200);
+        response.end(html);
+        })
+  }else   //메인화면이 아닌페이지
+    {
+      fs.readdir('./data', function(error, filelist){
+        var filteredid = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredid}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.html(title, list,
+             `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>
+              <form action="/delete_process" method="post" onsubmit="real??">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>
+              `
+          );
+        response.writeHead(200);
+        response.end(html);
+      });
+    });
+  }
+
+}else if(pathname === '/create'){
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB-create';
+    var list = template.list(filelist);
+    var html = template.html(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </P>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,'');
+  response.writeHead(200);
+  response.end(html);
+});
+}else if(pathname === '/create_process'){
+  var body = '';
+  request.on('data', function(data){
+    body = body + data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.writeHead(302, {Location: `/?id=${title}`});  //입력과동시에 해당페이지로이동
+      response.end('success');
+    })
+  });
+}else if(pathname === '/update'){
+  fs.readdir('./data', function(error, filelist){
+      var filteredid = path.parse(queryData.id).base;
+    fs.readFile(`data/${filteredid}`, 'utf8', function(err, description){
+      var title = queryData.id;
+      var list = template.list(filelist);
+      var html = template.html(title, list, `
+          <form action="/update_process" method="post">
+          <input type="hidden" name='id' value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </P>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+    response.writeHead(200);
+    response.end(html);
+  });
+});
+} else if(pathname === '/update_process'){
+  var body = '';
+  request.on('data', function(data){
+    body = body + data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error){
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+         response.writeHead(302, {Location: `/?id=${title}`});  //입력과동시에 해당페이지로이동
+         response.end();
+       })
+    })
+  });
+}else if(pathname === '/delete_process'){
+  var body = '';
+  request.on('data', function(data){
+    body = body + data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var id = post.id;
+    fs.unlink(`data/${id}`, function(error){
+      response.writeHead(302, {Location: `/`});  //입력과동시에 해당페이지로이동
+      response.end();
+    })
+  });
+}else{
+  response.writeHead(404);
+  response.end('Not found');
+}
+});
+app.listen(3000);
